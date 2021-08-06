7795b99e2dc06b409e6a544d3504e3ef4f1bb167
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-08-05 22:19:53 -0300

    Add project files.

---
+++ Brafe_v01-Origamid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Brafe_v01_Origamid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
