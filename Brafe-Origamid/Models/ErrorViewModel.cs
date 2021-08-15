805ad7083a15b488c3b386044706752f90df031b
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2021-08-15 01:02:25 -0300

    projeto utilizando somente o css para fazer o layout da hme page.

---
+++ Brafe-Origamid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Brafe_Origamid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
